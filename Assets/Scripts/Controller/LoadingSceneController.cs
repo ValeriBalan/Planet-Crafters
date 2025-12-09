8740a09f1b869a9739504b2bd7203376d0ebce34
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2025-12-09 08:59:08 +0200

    Add initial scene management and UI prefabs
    Added new scenes for boot, loading, main planet, registration, sign-in, and sign-up. Introduced SceneLoader and LoadingSceneController scripts for scene management. Added AuthSceneButton UI script and a sign-up button prefab. Updated project files and build settings to include new assets.

---
+++ Assets/Scripts/Controller/LoadingSceneController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    public static LoadingSceneController Instance;
+
+    public float exitDelay = 0.3f;
+
+    Action onFadeOutComplete;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        SceneLoader.Instance.StartAsyncLoad();
+    }
+
+    public void OnSceneReady(Action callback)
+    {
+        onFadeOutComplete = callback;
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    IEnumerator FadeOutRoutine()
+    {
+        yield return new WaitForSeconds(exitDelay);
+
+        onFadeOutComplete?.Invoke();
+    }
+}
